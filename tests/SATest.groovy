7c1b3c085c9b1470f41c9be2bea9c127eac9395f
luizcelso <gomesjr@ic.unicamp.br>
2012-12-24 13:55:15 -0200

    before changes in proj structure

---
+++ tests/SATest.groovy
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: luizcelso
+ * Date: 12/24/12
+ * Time: 1:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class SATest{// extends GroovyTestCase{
+    private sa
+
+    void setUp(){
+        println "setuuup"
+    }
+
+    void testSA(){
+        assertEquals("hi", "hi")
+    }
+}
