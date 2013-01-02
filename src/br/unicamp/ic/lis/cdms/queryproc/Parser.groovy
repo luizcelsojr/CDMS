b78065addb46082e69db487df585f2c22fe85066
luizcelso <gomesjr@ic.unicamp.br>
2013-01-02 09:35:47 -0200

    added parser and walker skeletons

---
+++ src/br/unicamp/ic/lis/cdms/queryproc/Parser.groovy
@@ -0,0 +1,15 @@
+package br.unicamp.ic.lis.cdms.queryproc
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: luizcelso
+ * Date: 1/1/13
+ * Time: 11:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Parser {
+    def parse(){
+        def queryConfig = ""
+        return queryConfig
+    }
+}
---
+++ src/br/unicamp/ic/lis/cdms/randomwalker/RW.groovy
@@ -0,0 +1,26 @@
+package br.unicamp.ic.lis.cdms.randomwalker
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: luizcelso
+ * Date: 1/1/13
+ * Time: 3:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class RW {
+    /*
+
+    for i in
+
+     */
+
+    def step(){
+
+    }
+
+    def start(){
+
+    }
+
+
+}
