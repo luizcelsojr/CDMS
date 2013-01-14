8c79e0b8ff94a131e1e6c5f7b395ecbce970897d
luizcelsojr <luizcelsojr@yahoo.com.br>
2013-01-14 17:20:38 -0200

    working on mapper

--- src/br/unicamp/ic/lis/cdms/mappers/LuceneMapper.groovy
+++ src/br/unicamp/ic/lis/cdms/mappers/LuceneMapper.groovy
@@ -1,5 +1,8 @@
 package br.unicamp.ic.lis.cdms.mappers
 
+import com.tinkerpop.blueprints.TransactionalGraph
+import com.tinkerpop.blueprints.Vertex
+import com.tinkerpop.gremlin.groovy.Gremlin
 import org.apache.lucene.analysis.standard.StandardAnalyzer
 import org.apache.lucene.queryParser.QueryParser
 import org.apache.lucene.util.Version
@@ -11,11 +14,65 @@ import org.apache.lucene.util.Version
  * To change this template use File | Settings | File Templates.
  */
 class LuceneMapper {
+    TransactionalGraph graph = null
+    def idxName = this.class.simpleName + 'Idx'
+    def index = null
+
     def analyser = new StandardAnalyzer(Version.LUCENE_36)
     def parser = new QueryParser(Version.LUCENE_36, "", this.analyser)
 
+    LuceneMapper (TransactionalGraph g) {
+        this.graph = g
+
+
+        if (!(this.index = this.graph.getIndex(this.idxName, Vertex.class))){
+            this.index = this.graph.createIndex(this.idxName, Vertex.class)
+
+        }
+
+    }
+
+    Vertex map (Vertex node){
+        Gremlin.load()
+        def tokenNode
+
+        def content = node.outE('http://www.w3.org/2000/01/rdf-schema#label').inV.next().value
+
+        if (!content) return null
+
+        this.parser.parse(content).each{
+            def value = it.toString()
+            def inode
+            def nodes = this.index.get('token', value)
+
+            nodes.each{inode = it}
+
+
+            if (!inode) {
+                inode = this.graph.addVertex()
+                inode.token = value
+                this.index.put('token', inode.token, inode)
+            }
+
+            this.graph.addEdge(node, inode, this.idxName + 'hasToken')
+
+        }
+
+        return node
+
+
+    }
+
+    void commit(){
+        this.graph.stopTransaction(TransactionalGraph.Conclusion.SUCCESS)
+    }
+
+    void rollback(){
+        this.graph.stopTransaction(TransactionalGraph.Conclusion.FAILURE)
+    }
+
     def run(){
-        println this.parser.parse("yeah babe boo:judie foster slence of lambs")
+
     }
 }
 
--- tests/MapperTest.groovy
+++ tests/MapperTest.groovy
@@ -7,11 +7,59 @@
  */
 
 import br.unicamp.ic.lis.cdms.mappers.*
+import com.tinkerpop.blueprints.impls.neo4j.Neo4jGraph
+import com.tinkerpop.gremlin.groovy.Gremlin
+import org.neo4j.graphdb.GraphDatabaseService
+import org.neo4j.graphdb.factory.GraphDatabaseFactory
 
 class MapperTest extends GroovyTestCase {
 
+    def graph = null
 
-    def testLucene (){
+    void setUp(){
+
+        def db_path = '/Users/luizcelso/db/LMDBsample'
+        def neoGraphDB = new GraphDatabaseFactory().newEmbeddedDatabase(db_path)
+        this.graph = new Neo4jGraph(neoGraphDB)
+
+        registerShutdownHook( neoGraphDB );
 
     }
+
+
+    void testLucene (){
+        Gremlin.load()
+        def mapper = new LuceneMapper(this.graph)
+
+        mapper.map(this.graph.v(4))
+        mapper.index.get('token', 'silence').each{println "hello ${it}"}
+
+        this.graph.v(4).outE.map().each{
+            println it
+        }
+
+        mapper.rollback()
+        print mapper.index.getIndexName()
+        mapper.index.get('token', 'silence').each{println "wont say ${it}"}
+
+
+
+    }
+
+
+    private static void registerShutdownHook( final GraphDatabaseService graphDb )
+    {
+        // Registers a shutdown hook for the Neo4j instance so that it
+        // shuts down nicely when the VM exits (even if you "Ctrl-C" the
+        // running example before it's completed)
+        Runtime.getRuntime().addShutdownHook( new Thread()
+        {
+            @Override
+            public void run()
+            {
+                graphDb.shutdown();
+            }
+        } );
+    }
+
 }
