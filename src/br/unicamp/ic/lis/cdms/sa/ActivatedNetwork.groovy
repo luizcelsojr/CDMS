36c78bbbcbc1fd5e602260095f8f46d67d41073c
luizcelsojr <luizcelsojr@yahoo.com.br>
2013-02-02 16:55:39 -0200

    working on Activated Network

---
+++ src/br/unicamp/ic/lis/cdms/sa/ActivatedNetwork.groovy
@@ -0,0 +1,11 @@
+package br.unicamp.ic.lis.cdms.sa
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: luizcelso
+ * Date: 2/1/13
+ * Time: 2:23 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class ActivatedNetwork {
+}
